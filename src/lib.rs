ead8e6619735ec0dac8340ad6a6555c1835182cf
toor <guiennet.jonathan@gmail.com>
2020-05-31 18:31:36 +0200

    base acyclic graph computing

---
+++ src/lib.rs
@@ -0,0 +1,122 @@
+use petgraph::Graph;
+use std::sync::Arc;
+
+//todo use Arg<T> intsead of Vec<T>
+pub type Job<T> = dyn Fn(Vec<T>) -> T + Send + Sync + 'static;
+pub type ExecutableGraph<'a, T> = Graph<Arc<Job<T>>, Option<T>>;
+
+pub mod thread {
+    use std::thread::JoinHandle;
+    use petgraph::graph::NodeIndex;
+    use std::sync::{Arc, Mutex, mpsc};
+    use std::sync::mpsc::{Sender, Receiver};
+    use std::thread;
+    use petgraph::Graph;
+    use crate::{Job, ExecutableGraph};
+
+    #[derive(Debug)]
+    pub struct Result<T> {
+        pub node_id: NodeIndex,
+        pub value: T,
+    }
+
+    enum Message<T> {
+        Operation(Operation<T>),
+        Terminate
+    }
+
+    pub struct Operation<T>{
+        pub node_id: NodeIndex,
+        pub data: Vec<T>,
+        pub f: Arc<Job<T>>
+    }
+    impl<T> Operation<T>{
+        pub fn new(node_id: NodeIndex, data: Vec<T>, g: &ExecutableGraph<T>) -> Operation<T>{
+            Operation{
+                node_id,
+                data,
+                f: Arc::clone(&g[node_id])
+            }
+        }
+    }
+
+    struct Worker {
+        thread: Option<JoinHandle<()>>
+    }
+
+    impl Worker {
+        fn new<T: Send + 'static>(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message<T>>>>, sender: Sender<Result<T>>)
+                                  -> Worker {
+            let thread = thread::spawn(move || loop {
+                let receiver = Arc::clone(&receiver);
+                let msg = receiver.lock().unwrap().recv().unwrap();
+                match msg {
+                    Message::Operation(Operation{node_id, data, f}) => {
+                        println!("Worker {} executing {:?}", id, node_id);
+                        sender.send(Result {
+                            node_id,
+                            value: f(data)
+                        });
+                    }
+                    Message::Terminate => break
+                }
+
+            });
+            Worker {
+                thread: Some(thread),
+            }
+
+        }
+    }
+
+    pub struct ThreadPool<T: Send> {
+        workers: Vec<Worker>,
+        receiver_thread_result: Receiver<Result<T>>,
+        sender: Sender<Message<T>>
+    }
+
+    impl<T: Send> Drop for ThreadPool<T>{
+        fn drop(&mut self) {
+            for _ in &self.workers {
+                self.sender.send(Message::Terminate).unwrap();
+            }
+
+            for worker in &mut self.workers {
+                if let Some(thread) = worker.thread.take(){
+                    thread.join().unwrap();
+                }
+            }
+        }
+    }
+
+    impl<T: Send + 'static> ThreadPool<T>{
+        pub fn new(size: usize) -> ThreadPool<T>{
+            assert!(size > 0);
+            let (sender, receiver) = mpsc::channel();
+            let (sender_thread_result, receiver_thread_result) = mpsc::channel();
+            let receiver = Arc::new(Mutex::new(receiver));
+            let mut workers = Vec::with_capacity(size);
+            for id in 0..size {
+                workers.push(Worker::new(
+                    id,
+                    Arc::clone(&receiver),
+                    sender_thread_result.clone()
+                ));
+            }
+            ThreadPool {
+                workers,
+                receiver_thread_result,
+                sender
+            }
+        }
+
+        pub fn execute(&self, operation: Operation<T>) {
+            self.sender.send(Message::Operation(operation)).unwrap();
+        }
+
+        pub fn get_receiver(&self) -> &Receiver<Result<T>>{
+            &self.receiver_thread_result
+        }
+    }
+}
+
